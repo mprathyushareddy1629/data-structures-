d09ff2bad31d41f1968dbb4d32ffe75dc7af21a6
mprathyushareddy1629 <47237484+mprathyushareddy1629@users.noreply.github.com>
2019-09-13 11:03:37 +0530

    Add files via upload

---
+++ pgm12prathu.c
@@ -0,0 +1,32 @@
+//insertion sort
+
+#include<stdio.h>
+int main()
+{
+	int n,array[1000],c,d,t;
+	printf("enter the number of elements \n");
+	scanf("%d",&n);
+	printf("enter %d integers \n",n);
+	for(c=0;c<n;c++)
+	{
+		scanf("%d",&array[c]);
+	}
+	for(c=1;c<=n-1;c++)
+	{
+		d=c;
+		while(d>0 && array[d-1]>array[d])
+		{
+			t=array[d];
+			array[d]=array[d-1];
+			array[d-1]=t;
+			d--;
+		}
+	}
+	printf("sorted list in ascending order:\n");
+	for(c=0;c<=n-1;c++)
+	{
+		printf("%d\n",array[c]);
+	}
+	return 0;
+}
+
